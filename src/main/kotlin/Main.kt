cd2fb42389df6f171eb45cf9ab18e575bb4998b2
bill chipman <wrchipman@gmail.com>
2023-09-18 21:35:04 -0300

    Second function completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
     val number = if(args.isNotEmpty()) args[0].toIntOrNull() else null
 
     println("$number is ${evenOrOdd(number)}")
+    println("$number! is ${calculateFactorial(number)}")
 }
 
 fun evenOrOdd(num:Int?): String {
@@ -13,4 +14,22 @@ fun evenOrOdd(num:Int?): String {
         }
     }
     return "Invalid input. Please enter a valid number."
+}
+
+fun calculateFactorial(num: Int?): Long? {
+    return when (num) {
+        null -> {
+            null
+        }
+        0, 1 -> {
+            1L
+        }
+        else -> {
+            var result = 1L
+            for (i in 2..num) {
+                result *= i
+            }
+            return result
+        }
+    }
 }
